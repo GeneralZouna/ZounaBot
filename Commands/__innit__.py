684f96f0d52f26d47f1c9eb42513e9b400919a03
GeneralZouna <39312243+GeneralZouna@users.noreply.github.com>
2021-12-16 00:49:56 +0100

    Delete __innit__.py

--- Commands/__innit__.py
+++
@@ -1,6 +0,0 @@
-import os
-__all__ = []
-for Lib in os.listdir("./Commands"):
-    if Lib.endswith(".py"):
-        if Lib != "__init__.py" and Lib != " ":
-            __all__.append(Lib.rstrip('.py'))
